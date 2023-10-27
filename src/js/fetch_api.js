74dc43e2319b328655535d931caec8727588c9a7
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-10-27 23:28:13 +0200

    Added infinite scroll

--- src/js/fetch_api.js
+++ src/js/fetch_api.js
@@ -2,19 +2,26 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 
 //<------------------------------------------------------------
-const MY_KEY = '40227453-3557d8d2139416ae0b447ea7a';
-const URL = 'https://pixabay.com/api/';
-
-//<------------------------------------------------------------
 export async function fetchData(searchQuery, page) {
+  const MY_KEY = '40227453-3557d8d2139416ae0b447ea7a';
+  const URL = 'https://pixabay.com/api/';
+  const params = {
+    key: MY_KEY,
+    q: searchQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+    page: page,
+  };
+  if (!searchQuery) {
+    return;
+  }
   try {
-    const responce = await axios.get(
-      `${URL}?key=${MY_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-    );
+    const responce = await axios.get(`${URL}`, { params });
 
     return responce.data;
   } catch (error) {
     Notiflix.Notify.failure('Щось пішло не так в "fetchData"');
   }
 }
-//<------------------------------------------------------------
